e9a0d55ceba678c474d165391cc3a3116707fc10
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-03-17 15:53:48 +0300

    Classes are added and Quizzler completed.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:quizzler/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Quizzler());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
